f2a0d3a4924c92d882b57690a9c1a72dd0fa3b72
Abhishek Gupta <abhishek9661342993@gmail.com>
2024-12-22 11:21:49 +0530

    fear: 📱  Responsive Nav and Hero
    - Add Toggle_Open and  Toggle_Close for Responsive Device
    - make Nav Responsive
    -  Change Logo
    - Make Notice Card Responsive
    - Add Responsive Design will me Modified becouse Some Component are not Fully Responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,14 +4,14 @@ import Notice_Card from "./Notice_Card";
 
 const Hero = () => {
   return (
-    <section className='w-full h-screen flex items-center justify-center bg-[url(https://s3-alpha-sig.figma.com/img/3a79/821c/cb304d66d7adb6b2dc98a09e6c614654?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=YtwlaGzBkgP1Yc4j2a1jKDX~BgoVIKxVWjK8IADR4mPr~1jgE5ygGq8MQTH9OPnq4HOdYD~smRlBVwSxlG0sSIg0QEaA5rWEviv-wvkF1STISrcxGZWr0eIMCKF2CTMSaU1uR3HSdhjULsn17wtNvCBHhjV41f~pgqkNInT7Mnd3gyDfP67J6NVvfp9dm-8lSu-ukTLPRSaXdnOCz~OvcfABgqp0zD2ZDZSi28iEY5vj7Er4ZxXUQNN1MisKNDZxxJYaSWNixEOG1IeP-2hUbnKcGTNmPlMMvT5eMOLfljsCh69lSvSIwuJGMdR7fANdQHcOXcajVRHmbxJau7U3fg__)] text-white bg-cover px-10'>
-      <div className="text w-1/2 h-full flex items-start justify-center flex-col">
-        <h3 className='text-6xl font-[700] leading-tight'>Shape Tomorrow</h3>
-        <h3 className='text-6xl font-[700] leading-tight'>with Quality</h3>
-        <h3 className='text-6xl font-[700] leading-tight'>Education</h3>
+    <section className='w-full h-screen flex flex-col lg:flex-row items-center justify-center  bg-[url(https://s3-alpha-sig.figma.com/img/3a79/821c/cb304d66d7adb6b2dc98a09e6c614654?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=YtwlaGzBkgP1Yc4j2a1jKDX~BgoVIKxVWjK8IADR4mPr~1jgE5ygGq8MQTH9OPnq4HOdYD~smRlBVwSxlG0sSIg0QEaA5rWEviv-wvkF1STISrcxGZWr0eIMCKF2CTMSaU1uR3HSdhjULsn17wtNvCBHhjV41f~pgqkNInT7Mnd3gyDfP67J6NVvfp9dm-8lSu-ukTLPRSaXdnOCz~OvcfABgqp0zD2ZDZSi28iEY5vj7Er4ZxXUQNN1MisKNDZxxJYaSWNixEOG1IeP-2hUbnKcGTNmPlMMvT5eMOLfljsCh69lSvSIwuJGMdR7fANdQHcOXcajVRHmbxJau7U3fg__)] text-white bg-cover bg-no-repeat px-10'>
+      <div className="text w-full lg:w-1/2 h-full flex items-start justify-end lg:justify-center flex-col">
+        <h3 className='text-[7vw] lg:text-[4vw] font-extrabold lg:font-[700] leading-tight'>Shape Tomorrow</h3>
+        <h3 className='text-[7vw] lg:text-[4vw] font-extrabold lg:font-[700] leading-tight'>with Quality</h3>
+        <h3 className='text-[7vw] lg:text-[4vw] font-extrabold lg:font-[700] leading-tight'>Education</h3>
         <p className='text-2xl w-[70%] '>Empowering students to achieve academic success with professional resources</p>
       </div>
-      <div className="card-container h-full w-1/2 flex items-end justify-end pb-10 flex-col gap-4">
+      <div className="card-container h-1/2 lg:h-full w-full lg:w-1/2 flex items-end justify-end pb-10 flex-col gap-4">
     
 
 
