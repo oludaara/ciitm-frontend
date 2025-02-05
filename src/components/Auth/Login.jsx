f2595e0366f99879395214ae4a4fe77ffdb23903
Abhishek Kumar <abhishek9661342993@gmail.com>
2025-02-05 22:40:07 +0530

    refactor: Enhance Login and Signup UI with responsive design and form styling (#91)
    Co-authored-by: Dev-Sameer-Khan <khansameer84233@gmail.com>
    Co-authored-by: Sameer Khan <131584949+Dev-Sameer-Khan@users.noreply.github.com>

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 const Login = () => {
   return (
     <section className='w-full min-h-screen flex items-center justify-center text-[#333] max-[999px]:flex-col'>
-      <div className='left w-1/2 max-[999px]:w-full h-full max-[999px]:px-6 max-[999px]:pt-20 max-[999px]:pb-10 px-20 flex flex-col items-center justify-center'>
+      <div className='left w-1/2 max-[999px]:w-full h-full max-[999px]:px-6 px-16 flex flex-col items-center justify-center'>
+        <form className='w-full h-full bg-[#FAFAFA] border-[1px] border-[#D7D7D7] p-6 rounded-xl drop-shadow-2xl'>
         <div className="text mb-4">
           <h1 className='text-[1.4vw] max-[999px]:text-[4.5vw] font-semibold font-[Poppins]'>Welcome Back</h1>
           <p className='text-[1vw] max-[999px]:text-[3vw]'>Allows you to pay online at all applications and website that accepts MasterCard cards</p>
         </div>
-        <form className='w-full h-full'>
         <label htmlFor="email" className='text-[1vw] my-2 text-[#5F5F5F] max-[999px]:text-[3vw]' >Email</label>
         <input type="email" id="email" className='border border-gray-300 rounded-lg p-3 mb-4 w-full text-[0.8vw] max-[999px]:text-[2.5vw]'/>
         <label htmlFor="password" className='text-[1vw] my-2 text-[#5F5F5F] max-[999px]:text-[3vw]' >Password</label>
@@ -28,7 +28,7 @@ const Login = () => {
         </div>
         </form>
       </div>
-      <div className='right w-1/2 h-full max-[999px]:hidden'>
+      <div className='right w-1/2 h-screen max-[999px]:hidden'>
         <img
           className='w-full h-full object-right object-cover pointer-events-none'
           src='https://s3-alpha-sig.figma.com/img/e725/38b8/a9f7d28b31be66c9f397e66d44dc014a?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=pp-4heoCh9xd8YPItETDX~QTN~d0oDajeXwHshV9LANgMJ2TCbzVOjuQJXswljm1mSY8n-TBWOTLDXzrWye73unLce~c3hKEFVIeKUuxLxDluzjUbgVsHHC51tiRdwKVmt0zaH9Z5ds7VsUC4JwetWFPvNcCPFJIJvgVNog0XY~cSq~3fZyBW9Z2AdLzXyc6yxBHDMXhJvsP46oEFkuN-2QHjiAGNlqlTbS58OsHCgAlwJuNFSSHVyjDUiEpmvP1c2j-CKDZWJ1aDwt0bKSTw-ejUJlqkfUHgVMytTjri7m46ksdSMgC7WcSMKXBMZ5vwa5qurKRTyZREVOgOrHeXA__'
--- src/components/Auth/Signup.jsx
+++ src/components/Auth/Signup.jsx
@@ -4,18 +4,18 @@ import { Link } from 'react-router-dom';
 const Signup = () => {
   return (
     <section className='w-full min-h-screen flex max-[999px]:flex-col'>
-      <div className='left w-1/2 max-[999px]:w-full h-full '>
+      <div className='left w-1/2 max-[999px]:hidden h-full '>
         <img
           className='w-full h-full object-cover pointer-events-none'
           src='https://s3-alpha-sig.figma.com/img/9e53/f389/4c758a66dbf9be2bba871477a57a44ff?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=DE2qNFEd3fj5XMZeA4EYhLs4oUXu7VG9IjMEScYw6wkTQPaQtlKJI4o1z5cTda2gxD7HF93P2BX1TFR2ntcbdZj-7yNItn~PXbKmEXAjN4PFrj0S6ume~1t7sXBsf5aGhM9UPz8h76symhPdCE3RZT2wVc0OX9vb3ZsDG0nIXt73UcihxzNEQ5tF9kEN6oX5oCW9WvuQHB7k-PMZP4nAAIRyVO5aXYlRTreqfXh-u~ECWdl6oE2T5xRfUkrYjasdJwi0XQ2-jGg~bfvrNX1WV207krTE0UPECiEJtf5RUCcquoM2IE8CnmFNnODxC-K~I9S1DIdtRgKERQlH7un1qw__'
           alt=''
         />
       </div>
-      <div className='right w-1/2 max-[999px]:w-full h-full max-[999px]:px-6 max-[999px]:py-4 pt-32 px-20 flex flex-col items-start justify-center text-[#333]'>
-        <h1 className='heading text-[1.8vw] max-[999px]:text-[4.5vw] font-semibold my-6'>
+      <div className='right w-1/2 max-[999px]:w-full h-full max-[999px]:px-6 max-[999px]:pt-[50vw] pt-32 px-16 flex flex-col items-start justify-center text-[#333]'>
+        <form className='w-full h-full bg-[#FAFAFA] border-[1px] border-[#D7D7D7] p-6 rounded-xl drop-shadow-2xl'>
+        <h1 className='heading text-[1.8vw] max-[999px]:text-[4.5vw] font-semibold mb-6'>
           Let’s Go Started Together
         </h1>
-        <form className='w-full h-full'>
           <div className='w-full flex items-center justify-between gap-7'>
             <input
               type='text'
