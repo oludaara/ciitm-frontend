3e5c69206cf4d96ba28548017ef4464c3d48b4aa
Okeke Chinedu Emmanuel <chiscookeke11@gmail.com>
2025-02-17 16:54:31 +0100

    Fixed image overflow on the empower section (#109)
    * Fix
    
    * fix
    
    * new fix
    
    * fix Error page
    
    * FIX
    
    * fix image overflow
    
    * @fix@

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const backend_Url = 'http://localhost:3000';
+export const backend_Url = 'https://ciitm-backend.onrender.com/';
 export const Login_EndPoint = '/api/login';
 export const frontend_EndPoint = '/api/frontend';
 export const Testimonital_EndPoint = '/api/find/testimonial';
