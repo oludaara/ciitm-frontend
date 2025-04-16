f5958050b36d2e0dad011e887bc303284d647a17
Abhishek Kumar <abhishek9661342993@gmail.com>
2025-04-16 18:15:48 +0530

    fix: correct heading element from h2 to h3
    - update QuickLink styles for better responsiveness;
    - adjust QuickLinkSection heading color

--- src/components/Atoms/Links/QuickLink.jsx
+++ src/components/Atoms/Links/QuickLink.jsx
@@ -6,7 +6,7 @@ const QuickLink = ({ path, text }) => {
    return (
       <Link
          to={path}
-         className='text-[2vw] w-[15vw]  md:text-[1vw] text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-white inline  p-[0.6vw] md:w-[10vw]  border border-1 rounded-xl text-center'
+         className='text-[2vw] max-[640px]:text-[3vw] max-[640px]:p-[1vw] max-[992px]:text-[2.5vw] max-[640px]:w-[30vw] max-[992px]:w-[22vw]  md:text-[1vw] text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-white inline  p-[0.6vw] md:w-[10vw]  border border-1 rounded-xl text-center'
       >
          {text}
       </Link>
